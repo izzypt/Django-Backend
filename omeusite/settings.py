9b9f45dabe5c1f7019dc954a432e01cc709bf9e8
Simão Bonvalot <73948790+izzypt@users.noreply.github.com>
2022-01-03 21:27:51 +0000

    Update settings.py

--- omeusite/settings.py
+++ omeusite/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(tw=8ngi8!=v1%fkm(gdal=a3slq#jxlf-t0yo#_lkt3&+!27)'
-
+SECRET_KEY = 'USE YOUR OWN'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
